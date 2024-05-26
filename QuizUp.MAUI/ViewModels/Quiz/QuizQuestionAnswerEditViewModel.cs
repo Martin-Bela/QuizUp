acf8825fc1b73391d69c3cc818589459bb475bd9
Martin Beľa <xbela1@fi.muni.cz>
2024-05-27 00:02:49 +0200

    feat: Add functionality to editing quizes

--- QuizUp.MAUI/ViewModels/Quiz/QuizQuestionAnswerEditViewModel.cs
+++ QuizUp.MAUI/ViewModels/Quiz/QuizQuestionAnswerEditViewModel.cs
@@ -1,12 +1,62 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using QuizUp.MAUI.Services.Rest;
 
 namespace QuizUp.MAUI.ViewModels;
 
+[QueryProperty(nameof(Quiz), nameof(Quiz))]
+[QueryProperty(nameof(QuestionPos), nameof(QuestionPos))]
+[QueryProperty(nameof(AnswerPos), nameof(AnswerPos))]
 public partial class QuizQuestionAnswerEditViewModel(ViewModelBase.Dependencies dependencies) : ViewModelBase(dependencies)
 {
+    QuizDetailModel quiz = null!;
+    public QuizDetailModel Quiz
+    {
+        get => quiz;
+        set
+        {
+            quiz = value;
+            SetAnswer();
+        }
+    }
+
+    int questionPos = -1;
+    public int QuestionPos
+    {
+        get => questionPos;
+        set
+        {
+            questionPos = value;
+            SetAnswer();
+        }
+    }
+
+    int answerPos = -1;
+    public int AnswerPos
+    {
+        get => answerPos;
+        set
+        {
+            answerPos = value;
+            SetAnswer();
+        }
+    }
+
+    [ObservableProperty]
+    public AnswerDetailModel? answer;
+
+    private void SetAnswer()
+    {
+        if (Quiz is not null && QuestionPos >= 0 && AnswerPos >= 0)
+        {
+            Answer = Quiz.Questions[QuestionPos].Answers[AnswerPos];
+        }
+    }
+
+
     [RelayCommand]
     private async Task SaveAnswerAsync()
     {
-        await Shell.Current.GoToAsync("../");
+        await Shell.Current.GoToAsync("../", new Dictionary<string, object> { { "Quiz", Quiz } });
     }
 }
