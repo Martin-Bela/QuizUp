093300a5a2f65f4054967201050b8a45501ed327
Petr Cermak <x483005@fi.muni.cz>
2024-05-31 15:02:01 +0200

    refactor: styling

---
+++ QuizUp.MAUI/Converters/AnswerResultTextConverter.cs
@@ -0,0 +1,32 @@
+﻿using QuizUp.Common.Models;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace QuizUp.MAUI.Converters;
+public class AnswerResultTextConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        Debug.Assert(value != null);
+
+        var parsingSuceeded = Enum.TryParse<AnswerResult>(value.ToString(), out var answerResult);
+        if (!parsingSuceeded)
+        {
+            answerResult = (AnswerResult)(-1);
+        }
+
+        return AnswerResultToText(answerResult);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+    private static string AnswerResultToText(AnswerResult result) => result switch
+    {
+        AnswerResult.Correct => "Correct",
+        AnswerResult.Incorrect => "Wrong answer",
+        AnswerResult.TimeExpired => "Time is up",
+        _ => "You are doing great!"
+    };
+}
